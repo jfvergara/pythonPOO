a154fde8a86be707f5fffcf317cfcd37fbbc1b17
Jose Francisco Vergara Espinoza <j.f.vergara@cgiar.org>
2021-06-09 21:58:01 -0500

    feat: Print the project names using a for loop

--- AIRequest.py
+++ AIRequest.py
@@ -1,3 +1,6 @@
 import requests
 response = requests.get("https://gitlab.com/api/v4/users/nanuchi/projects")
-print(response.json())
+my_projects = response.json()
+
+for project in my_projects:
+    print(f"Project Name: {project['name']}")
